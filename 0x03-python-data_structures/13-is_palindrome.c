ba938f956c21c899581e6eb76f4aa7300c7e2e2f
dina <dinaelhadidy966@gmail.com>
2023-05-16 13:04:11 -0400

    Insert newline at the end of switch.py and Upload is_palindrome task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reverseList - reverse linked list
+ * @ptr: pointer to the first node in list
+ *
+ * Return: previous pointer
+ */
+listint_t *reverseList(listint_t *ptr)
+{
+	listint_t *prev = NULL;
+
+	while (ptr)
+	{
+		listint_t *next = ptr->next;
+
+		ptr->next = prev;
+		prev = ptr;
+		ptr = next;
+	}
+	return (prev);
+}
+/**
+ * is_palindrome - checks if linked list is palindrome or not
+ * @head: pointer to pointer to head
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+		return (0);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	listint_t *fast = *head, *slow = *head, *temp = *head;
+
+	while (fast->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	slow->next = reverseList(slow->next);
+	slow = slow->next;
+	while (slow)
+	{
+		if (temp->n != slow->n)
+			return (0);
+		temp = temp->next;
+		slow = slow->next;
+	}
+	return (1);
+}
