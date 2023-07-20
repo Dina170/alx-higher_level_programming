9cb76a1d96eb7c7e59f1d78760dd826c052c2f8b
dina <dinaelhadidy966@gmail.com>
2023-07-20 17:48:45 -0400

    Fix ON condition

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,9 +3,9 @@
 SELECT name, SUM(rate) AS rating
   FROM tv_genres
        INNER JOIN tv_show_genres
-       ON tv_show_genres.genre_id = tv_genres.id
+       ON tv_genres.id = tv_show_genres.genre_id 
 
        INNER JOIN tv_show_ratings
-       ON tv_genres.show_id = tv_show_genres.show_id
+       ON tv_show_genres.show_id = tv_show_ratings.show_id
  GROUP BY name
  ORDER BY rating DESC;
