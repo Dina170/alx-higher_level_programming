48f9e13247231ee5c49be5a0284fbd6ebc4a5c39
dina <dinaelhadidy966@gmail.com>
2023-08-19 15:19:35 -0400

    Add LIKE BINARY operator and fix tuple in execute function

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-Takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
+Takes in an argument and displays all values in the states
+where `name` matches the argument from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -15,13 +13,12 @@ if __name__ == '__main__':
     connect to the database and filter
     results from the database.
     """
-
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name = '{}' \
+                 WHERE BINARY name = '{}' \
                  ORDER BY states.id".format(argv[4]))
     rows = cur.fetchall()
 
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,14 +18,14 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name \
+    cur.execute("SELECT cities.name \
                    FROM cities \
                    JOIN states \
                      ON cities.state_id = states.id \
                   WHERE states.name = '%s' \
-                  ORDER BY cities.id", argv[4])
+                  ORDER BY cities.id", (argv[4],))
 
     rows = cur.fetchall()
 
     if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+        print(", ".join([row[0] for row in rows]))
