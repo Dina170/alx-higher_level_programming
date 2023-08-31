4499aeb0604d100889fb44543f39c611d26943de
dina <dinaelhadidy966@gmail.com>
2023-08-31 12:37:54 -0400

    Fix pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,11 +5,10 @@
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers."""
     if list_of_integers == []:
-    	return None
-
+        return None
     low = 0
     high = len(list_of_integers) - 1
-    
+
     while low < high:
         mid = low + (high - low) // 2
 
@@ -17,5 +16,4 @@ def find_peak(list_of_integers):
             high = mid
         else:
             low = mid + 1
-    
     return list_of_integers[low]
